b999be9705be5553d9681c480ecce7c7b1ab4909
MauMau1232 <bessemarvin042@gmail.com>
2025-08-30 14:08:42 +0200

    Unused Ranks

---
+++ src/ranks/half.lua
@@ -0,0 +1,32 @@
+SMODS.Rank {
+    key = 'HalfJack',
+    card_key = 'HJ',
+    pos = { x = 0 },
+    nominal = 7.5,
+    face = true,
+    shorthand = 'HJ',
+    next = { 'cstorm_HalfQueen' },
+    atlas = "halfRanks",
+}
+
+SMODS.Rank {
+    key = 'HalfQueen',
+    card_key = 'HQ',
+    pos = { x = 1 },
+    nominal = 7.5,
+    face = true,
+    shorthand = 'HQ',
+    next = { 'cstorm_HalfKing' },
+    atlas = "halfRanks"
+}
+
+SMODS.Rank {
+    key = 'HalfKing',
+    card_key = 'HK',
+    pos = { x = 2 },
+    nominal = 7.5,
+    face = true,
+    shorthand = 'HK',
+    next = { 'Ace' },
+    atlas = "halfRanks"
+}
