f8922ef64d1e30d82d31d46a044eefb88cfa777a
MauMau1232 <bessemarvin042@gmail.com>
2025-08-30 14:09:32 +0200

    Added the Joker Chipy

---
+++ src/jokers/chipy.lua
@@ -0,0 +1,33 @@
+SMODS.Joker{
+    key = "chipy",
+    config = { extra = { chips = 50, chipsMod = 50 } },
+    pos = { x = 3, y = 1 },
+    rarity = 1,
+    cost = 4,
+    blueprint_compat = true,
+    eternal_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'jokers',
+
+    calculate = function (self, card, context)
+        if context.joker_main then
+            return {
+                chips = card.ability.extra.chips
+            }
+        end
+
+        if context.buying_card and not context.blueprint_card then
+           if context.card.ability.set == "Joker" then
+            card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chipsMod
+            return {
+                extra = {focus = context.self, colour = G.C.CHIPS, message = localize('k_upgrade_ex')}
+            }
+           end
+        end
+    end,
+
+    loc_vars = function (self, info_queue, card)
+        return { vars = { card.ability.extra.chips, card.ability.extra.chipsMod}, key = self.key }
+    end
+}
