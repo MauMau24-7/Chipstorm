9b1e39eafb3682fcb4daaaf607be9293c0037573
MauMau1232 <bessemarvin042@gmail.com>
2025-09-04 02:51:54 +0200

    fixes

--- src/rarities/chatter.lua
+++ src/rarities/chatter.lua
@@ -1,6 +1,7 @@
 SMODS.Rarity{
     key = "chatter",
     pools = {
+        ["Joker"] = true,
         ["Joker"] = { rate = 0.7 },
     },
     badge_colour = HEX("8956FB"),
--- src/tags/idk.lua
+++
@@ -1,9 +0,0 @@
-SMODS.Tag{
-    key = "dog",
-    --atlas = "something",
-    config = {  },
-    pos = { x = 0, y = 0 },
-    discovered = true,
-    unlocked = true,
-    min_ante = 0,
-}
